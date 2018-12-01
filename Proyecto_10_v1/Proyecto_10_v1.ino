c1fd78e7e1cf6c2914f05a1bff534f63625e926c
RazorbreakRZ <razorbreak@gmail.com>
2018-12-01 22:59:40 +0100

    Renamed file

--- Proyecto_10_v1/Proyecto_10_v1.ino
+++ Proyecto_10_v1/Proyecto_10_v1.ino
@@ -22,3 +22,40 @@ void setup() {
     pinMode(enablePin, OUTPUT);
     digitalWrite(enablePin, LOW);
 }
+
+void loop() {
+    onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
+    delay(1);
+    directionSwitchState = digitalRead(directionSwitchPin);
+    motorSpeed = analogRead(potPin)/4;
+
+    if (onOffSwitchState != previousOnOffSwitchState) {
+        if (onOffSwitchState == HIGH) {
+            motorEnabled = !motorEnabled;
+        }
+    }
+
+    if (directionSwitchState != previousDirectionSwitchState) {
+        if (directionSwitchState == HIGH) {
+            motorDirection = !motorDirection;
+        }
+    }
+
+    if (motorDirection == 1) {
+        digitalWrite(controlPin1, HIGH);
+        digitalWrite(controlPin2, LOW);
+    } else {
+        digitalWrite(controlPin1, LOW);
+        digitalWrite(controlPin2, HIGH);
+    }
+
+    if (motorEnabled == 1) {
+        analogWrite(enablePin, motorSpeed);
+    } else {
+        analogWrite(enablePin, 0);
+    }
+
+    previousDirectionSwitchState = directionSwitchState;
+    previousOnOffSwitchState = onOffSwitchState;
+
+}
