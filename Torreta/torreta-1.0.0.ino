4a761d084edb7a9245e9f6db319c81423f146c32
RazorbreakRZ <razorbreak@gmail.com>
2019-03-08 15:04:17 +0100

    Sample program with serial data.

--- Torreta/torreta-1.0.0.ino
+++ Torreta/torreta-1.0.0.ino
@@ -1,4 +1,12 @@
+#include <math.h>
 
-void setup() {
-	Serial.begin(9600)
+void setup() {  
+	randomSeed(analogRead(0));
+	Serial.begin(9600);
+}
+
+void loop() {
+	int data = random(0,255);
+	Serial.write(data); // data value must be 0-255
+	delay(1000);
 }
